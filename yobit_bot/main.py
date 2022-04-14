0499b8c477c39c1992eaf3a8ead4f208c99c08c0
pazuzu71 <genchez.tula@gmail.com>
2022-04-14 12:01:44 +0300

    Без комментариев

--- yobit_bot/main.py
+++ yobit_bot/main.py
@@ -1,4 +1,8 @@
+from config import token
+
 import requests
+from aiogram import Bot, Dispatcher, executor
+import asyncio
 
 
 def get_price_btc_usd():
@@ -8,6 +12,10 @@ def get_price_btc_usd():
     # print(price)
     return price
 
+
+bot = Bot(token, parse_mode="HTML")
+dp = Dispatcher(bot=bot)
+loop = asyncio.
 
 if __name__ == "__main__":
     print(get_price_btc_usd())
